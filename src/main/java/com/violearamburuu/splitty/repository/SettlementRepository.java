9b69745dd1b605704057fac7097a2b03793265b4
Violeta Aramburu <violearamburu@gmail.com>
2026-07-27 23:34:27 -0300

    feature: added SettlementRepository

---
+++ src/main/java/com/violearamburuu/splitty/repository/SettlementRepository.java
@@ -0,0 +1,11 @@
+﻿package com.violearamburuu.splitty.repository;
+
+import com.violearamburuu.splitty.model.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SettlementRepository extends JpaRepository<Settlement, Long> {
+    Optional<Settlement> findAllByGroup(Group group);
+    Optional<Settlement> findAllByFromUser(User user);
+}
