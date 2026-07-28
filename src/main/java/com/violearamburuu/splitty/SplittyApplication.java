bdfaeef2b867179ecbc58296341659ec865ef762
Violeta Aramburu <violearamburu@gmail.com>
2026-07-27 21:58:41 -0300

    chore: initial project setup with Spring Boot and dependencies

---
+++ src/main/java/com/violearamburuu/splitty/SplittyApplication.java
@@ -0,0 +1,13 @@
+package com.violearamburuu.splitty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplittyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SplittyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/violearamburuu/splitty/SplittyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.violearamburuu.splitty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplittyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
