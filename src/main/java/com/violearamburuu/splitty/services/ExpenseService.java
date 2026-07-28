4fafbb6391186414f942d44dbd03ba45176f3798
Violeta Aramburu <violearamburu@gmail.com>
2026-07-28 00:53:38 -0300

    feture: added ExpenseService

---
+++ src/main/java/com/violearamburuu/splitty/services/ExpenseService.java
@@ -0,0 +1,36 @@
+﻿package com.violearamburuu.splitty.services;
+
+import com.violearamburuu.splitty.model.*;
+import com.violearamburuu.splitty.repository.*;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Service
+public class ExpenseService {
+    private final ExpenseRepository expenseRepository;
+    private final UserRepository userRepository;
+    private final GroupMembershipRepository groupMembershipRepository;
+    private final GroupRepository groupRepository;
+
+    public ExpenseService(ExpenseRepository expenseRepository, UserRepository userRepository, GroupMembershipRepository groupMembershipRepository, GroupRepository groupRepository){
+        this.expenseRepository = expenseRepository;
+        this.userRepository = userRepository;
+        this.groupMembershipRepository = groupMembershipRepository;
+        this.groupRepository = groupRepository;
+    }
+
+    public Expense createExpense(User user, Group group, BigDecimal amount){
+        groupRepository.findByName(group.getName()).orElseThrow(() -> new RuntimeException("This group doesn't exist"));
+        groupMembershipRepository.findByUserAndGroup(user, group).orElseThrow(() -> new RuntimeException("This user does not belong to this group."));
+        if(amount.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new RuntimeException("This is not a valid amount.");
+        }
+        Expense newExpense = new Expense(amount, user, group);
+        expenseRepository.save(newExpense);
+        return newExpense;
+
+        // TERMINAR
+    }
+}
