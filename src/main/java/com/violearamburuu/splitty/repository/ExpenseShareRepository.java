53deaddf9a566f0d4d228cc175c25889291e0f5d
Violeta Aramburu <violearamburu@gmail.com>
2026-07-27 23:42:11 -0300

    feature: added ExpenseShareRepository

---
+++ src/main/java/com/violearamburuu/splitty/repository/ExpenseShareRepository.java
@@ -0,0 +1,9 @@
+﻿package com.violearamburuu.splitty.repository;
+
+import com.violearamburuu.splitty.model.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.*;
+
+public interface ExpenseShareRepository extends JpaRepository<ExpenseShare, Long> {
+    List<ExpenseShare> findAllByExpense(Expense expense);
+}
